e3fe5dfb72d7447709ff42be6fe94a4b0f25fcf9
cromes85 <idv230685@gmail.com>
2024-01-29 20:40:57 +0100

    new methode

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.post('/api/save-price', (req, res) => {
+    const ducoPrice = req.body.price;
+
+    if (!ducoPrice) {
+        return res.status(400).json({ error: 'Invalid data' });
+    }
+
+    // Charger les données existantes du fichier JSON
+    let existingData = [];
+    try {
+        const jsonData = fs.readFileSync('ducoPrices.json', 'utf-8');
+        existingData = JSON.parse(jsonData);
+    } catch (error) {
+        console.error('Error reading existing data:', error);
+    }
+
+    // Ajouter le nouveau prix aux données existantes
+    existingData.push({ timestamp: new Date().toISOString(), price: ducoPrice });
+
+    // Enregistrer les données mises à jour dans le fichier JSON
+    try {
+        fs.writeFileSync('ducoPrices.json', JSON.stringify(existingData));
+    } catch (error) {
+        console.error('Error writing data to file:', error);
+        return res.status(500).json({ error: 'Internal server error' });
+    }
+
+    res.json({ success: true });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
